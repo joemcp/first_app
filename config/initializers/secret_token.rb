5c3720b9802e307f5b8deff37f824d10f5f3226b
Joe McPherson <jmcpherson@nutrisystem.com>
2013-02-06 13:12:31 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '7826fe15c73e931775ef78829d4e09af8b61c49b3002dff48d236aed84d587b2014140abdaeeee039db564ba4319ee7e2daccc10bfe7a797e78000cb62751843'
